ce69bddf5a3b89a49ff9174abf1a7cf0caffb846
sushi-chaaaan <mail@sushichan.live>
2024-06-20 14:54:49 +0900

    fix openai

--- src/ai/type.ts
+++ src/ai/type.ts
@@ -1,14 +1,6 @@
 import z from "zod";
 
-// https://platform.openai.com/docs/models
-const OPENAI_MODELS = ["gpt-3.5-turbo", "gpt-4-turbo", "gpt-4o"] as const;
-const OPENAI_ACTUAL_MODEL_NAMES = {
-  "gpt-3.5-turbo": "gpt-3.5-turbo-0125",
-  "gpt-4-turbo": "gpt-4-turbo-2024-04-09",
-  "gpt-4o": "gpt-4o-2024-05-13",
-} as const satisfies {
-  [K in (typeof OPENAI_MODELS)[number]]: string;
-};
+import { OPENAI_ACTUAL_MODEL_NAMES, OPENAI_MODELS } from "./openai";
 
 interface AIContext {
   apiKey: string;
